cc537630e524f46971fb237065217c97fd1bcf0a
partha-cloud <partha11dey@gmail.com>
2021-03-16 20:33:37 +0530

    added UC3

--- Game.java
+++ Game.java
@@ -4,26 +4,33 @@ import java.util.*;
 
 public class Game {
 
-	public static void main(String[] args) {}
-	Scanner userInput=new Scanner(System.in);
-	
-		 char userLetter = chooseUserLetter1(userInput);
-		 char computerLetter = (userLetter == 'X') ? '0' : 'X';
-		 
-	
+	 public static void showBoard() {
+	        System.out.println("Currently the Board Looks Like ");
+	        System.out.println(" ");
+	        String[] board = null;
+			System.out.println("\n "+ board[1] + " | "+ board[2] + " | " + board[3]);
+	        System.out.println("-----------");
+	        System.out.println(" "+ board[4] + " | " + board[5] + " | " + board[6]);
+	        System.out.println("-----------");
+	        System.out.println(" "+ board[7] + " | " + board[8] + " | " + board[9]);
+	        System.out.println(" ");
+	    }
 
-  private static char chooseUserLetter1(Scanner userInput) {
-    	System.out.print("choose your letter: ");
-   return userInput.next().toUpperCase().charAt(0);
-    }
+	    public static void main(String[] args) {
+	        makeEmpty();
+	        playerSelect();
+	        showBoard();
+	        
+	    }
 
+		private static void makeEmpty() {
+			// TODO Auto-generated method stub
+			
+		}
 
-
-	private char chooseUserLetter(Scanner userInput2) {
-		// TODO Auto-generated method stub
-		return 0;
+		private static void playerSelect() {
+			// TODO Auto-generated method stub
+			
+		}
 	}
-
-	}
-
-
+	
