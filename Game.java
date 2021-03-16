818391f997b7d7c2cf75f90a0cceab20d3129e9d
partha-cloud <partha11dey@gmail.com>
2021-03-16 18:54:40 +0530

    added UC1

--- Game.java
+++ Game.java
@@ -3,17 +3,18 @@ package TIC;
 public class Game {
 
 	public static void main(String[] args) {
-		 char[] board=createBoard();
-    }
+		 char[] board1 = createBoard();
+		 System.out.println(board1);
+	}
 
     public static char[] createBoard()
     {
         char[] board=new char[10];
-        for(int index=0; index < board.length; index++)
+        for(int index=0; index < board.length ; index++)
         {
             board[index]=' ';
         }
-        return  board;
+        return board;
     }
 
 	}
