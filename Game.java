188b30580eed8de208fc5edc4a76e341cc6568d8
partha-cloud <partha11dey@gmail.com>
2021-03-19 23:30:35 +0530

    added UC7

--- Game.java
+++ Game.java
@@ -7,35 +7,129 @@ import java.util.Scanner;
 
 public class Game {
 
-	 public static void main(String[] args)
-	    {
+	  public static void main(String[] args) {
 	        System.out.println("--------------------Welcome To TicTocToe Game-------------------------");
 	        char[] board = createBoard();
 	        char UserLetter = chooseLetter();
 	        char ComputerLetter;
-	        if(UserLetter=='X')
+	        if (UserLetter == 'X')
 	        {
-	            ComputerLetter='O';
-	        }
-	        else
+	            ComputerLetter = 'O';
+	        } else
 	        {
-	            ComputerLetter='X';
+	            ComputerLetter = 'X';
 
 	        }
-	        System.out.println("Computer Letter is: " + ComputerLetter+" User Letter is : " + UserLetter);
+	        System.out.println("Computer Letter is: " + ComputerLetter + " User Letter is : " + UserLetter);
 	        System.out.println("Current Board:");
 	        showBoard(board);
-	        int first=toss();
-	        if(first==1)
+	        int first = toss();
+	        boolean win=false;
+	        String result=null;
+	        while (win==false)
 	        {
-	            getUserMove(board,UserLetter);
+	            if (first == 1)
+	            {
+	                getUserMove(board, UserLetter);
+	                first=2;
+	            }
+	            else
+	            {
+	                getUserMove(board, ComputerLetter);
+	                first=1;
+	            }
+	            System.out.println("Current Board:");
+	            showBoard(board);
+	            result=iswin(board);
+	            if(result=="X")
+	            {
+	                System.out.println("X is winner");
+	                win=true;
+	            }
+	            else if(result=="O")
+	            {
+	                System.out.println("O is winner");
+	                win=true;
+	            }
+	            else if(isdraw(board))
+	            {
+	                System.out.println("Game is draw");
+	                win=true;
+	            }
+
+	        }
+	    }
+	    public static boolean isdraw(char[] board)
+	    {
+	        int counter=0;
+	        for(int i=1;i<10;i++)
+	        {
+	            if(board[i]!=' ')
+	            {
+	                counter=counter+1;
+	            }
+	        }
+	        if(counter==9)
+	        {
+	            return true;
 	        }
 	        else
-	            {
-	                getUserMove(board,ComputerLetter);
+	        {
+	            return false;
 	        }
-	        System.out.println("Current Board:");
-	        showBoard(board);
+	    }
+	    public static String iswin(char[] board)
+	    {
+	        String temp=null;
+	        for (int choice = 0; choice < 8; choice++)
+	        {
+	            String line=null;
+
+	            switch (choice)
+	            {
+	                case 0:
+	                    line = Character.toString(board[1]) + Character.toString(board[2]) + Character.toString(board[3]);
+	                    break;
+	                case 1:
+	                    line = Character.toString(board[4]) + Character.toString(board[5]) + Character.toString(board[6]);
+	                    break;
+	                case 2:
+	                    line =Character.toString(board[7]) + Character.toString(board[8]) + Character.toString(board[9]);
+	                    break;
+	                case 3:
+	                    line = Character.toString(board[1]) + Character.toString(board[4]) + Character.toString(board[7]);
+	                    break;
+	                case 4:
+	                    line = Character.toString(board[2]) + Character.toString(board[5]) + Character.toString(board[8]);
+	                    break;
+	                case 5:
+	                    line = Character.toString(board[3]) + Character.toString(board[6]) + Character.toString(board[9]);
+	                    break;
+	                case 6:
+	                    line = Character.toString(board[1]) + Character.toString(board[5]) + Character.toString(board[9]);
+	                    break;
+	                case 7:
+	                    line = Character.toString(board[3]) + Character.toString(board[5]) + Character.toString(board[7]);
+	                    break;
+	            }
+	            //For X winner
+	            if (line.equals("XXX"))
+	            {
+	                temp= "X";
+	                break;
+	            }
+	            // For O winner
+	            else if (line.equals("OOO"))
+	            {
+	                temp= "O";
+	                break;
+	            }
+	            else
+	            {
+	                temp= "Play";
+	            }
+	        }
+	        return  temp;
 	    }
 	    public static int toss()
 	    {
@@ -101,4 +195,5 @@ public class Game {
 	        }
 	        return  board;
 	    }
-}
+	}
+	
